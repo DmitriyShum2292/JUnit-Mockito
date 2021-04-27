7c563a0d9c5f49ffcf7e967579461f1e4e8e8f97
Dmitriy Shumskiy <dmitriyshum2292@gmail.com>
2021-04-27 14:26:02 +0500

    added test for SecondController

--- src/main/java/com/example/tests/controller/SecondController.java
+++ src/main/java/com/example/tests/controller/SecondController.java
@@ -1,7 +1,37 @@
 package com.example.tests.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.tests.model.NewUser;
+import com.example.tests.service.NewUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/users")
 public class SecondController {
+
+    @Autowired
+    private NewUserService service;
+
+    private String message = "Not found!";
+
+    @GetMapping
+    public ArrayList<NewUser> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findOne(@PathVariable long id) throws Exception {
+        NewUser user = service.read(id);
+        if (user==null){
+            return new ResponseEntity(message,HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(user,HttpStatus.OK);
+    }
 }
---
+++ src/test/java/com/example/tests/controller/SecondControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.tests.controller;
+
+import junit.framework.TestCase;
+
+public class SecondControllerTest extends TestCase {
+
+    public void testFindAll() {
+    }
+
+    public void testFindOne() {
+    }
+}
