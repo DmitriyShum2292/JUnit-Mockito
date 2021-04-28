7096c857287decf1bd6ff4087975388767fdddba
Dmitriy Shumskiy <dmitriyshum2292@gmail.com>
2021-04-28 13:39:09 +0500

    added post method in SecondController

--- src/main/java/com/example/tests/controller/SecondController.java
+++ src/main/java/com/example/tests/controller/SecondController.java
@@ -5,10 +5,7 @@ import com.example.tests.service.NewUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -19,8 +16,6 @@ public class SecondController {
     @Autowired
     private NewUserService service;
 
-    private String message = "Not found!";
-
     @GetMapping
     public ArrayList<NewUser> findAll(){
         return service.findAll();
@@ -30,8 +25,16 @@ public class SecondController {
     public ResponseEntity findOne(@PathVariable long id) throws Exception {
         NewUser user = service.read(id);
         if (user==null){
-            return new ResponseEntity(message,HttpStatus.NOT_FOUND);
+            return new ResponseEntity("Not found!",HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity(user,HttpStatus.OK);
+    }
+    @PostMapping("/new")
+    public ResponseEntity create(@RequestBody NewUser user) throws Exception {
+        //NewUser user = new NewUser(username,password);
+        if(!service.create(user)){
+            return new ResponseEntity("User exists!",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity("Ok!",HttpStatus.OK);
     }
 }
